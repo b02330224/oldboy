5b355b09f7fafff116d8777381b5a614b1a436fb
weiqiang <240325184@qq.com>
2015-12-30 23:49:35 +0800

    update

--- day01/province/province.py
+++ day01/province/province.py
@@ -1,5 +1,7 @@
 #！/usr/bin/env python
 # -*- coding:utf-8 -*-
+import sys
+#将文件转换成字典信息
 def get_dict(info):
     with open(info,'r',encoding='utf8') as f:
         dict={}
@@ -19,29 +21,89 @@ def get_dict(info):
                 dict[province][city]=[]
                 dict[province][city].append(county)
     return dict
-def get_province(f):
+
+#获取各级信息列表函数
+def get_num_list(dict):
     n=1
-    global province_num_dict
-    for province in  f.keys():
-        print(n,province)
-        province_num_dict={}
-        province_num_dict[n]=province
+    num_dict={}
+    for province in  dict.keys():
+        num_dict[n]=province
         n += 1
-    return province_num_dict
-def get_city(p):
-    m = 1
-    for city in p.keys:
-        print(m,city)
-        city_dict={}
-        city_dict[m]=city
-        m +=1
-    return city_dict
+    return num_dict
+
+#测试输入是否为数字
+def test_input(test):
+    try:
+        int(test)
+    except ValueError:
+        return 1
 
 def main():
     entire_dict=get_dict('china.txt')
-    get_province(entire_dict)
-    pro=input('选择省份对应的编号')
-    print(entire_dict[province_num_dict[pro]].keys)
+    #获取省级列表字典
+    province_num_list=get_num_list(entire_dict)
+    #省级列表循环
+    while True:
+        flag='false'
+        for k,v in province_num_list.items():
+            print(k,v)
+        input_num=input('请输入对应省份的编号,退出输入q:').strip()
+        if input_num == 'q':
+            print('退出了！！！！')
+            sys.exit(0)
+        elif test_input(input_num)==1:
+            print('您输入的不是数字！！！')
+        elif int(input_num) not in province_num_list:
+            print('输入编号不在列表范围内！！！')
+        else:
+            #编号对应的省份：
+            p_name=province_num_list[int(input_num)]
+            #获取市级列表dict
+            city_num_list=get_num_list(entire_dict[p_name])
+            #进入市级列表循环
+            while flag=='false':
+                for n,m in city_num_list.items():
+                    print(n,m)
+                input_city_num=input('请输入对应城市的编号,输入b返回上级菜单，输入q退出:').strip()
+                if input_city_num=='q':
+                    print('退出了！！！')
+                    sys.exit(0)
+                elif input_city_num == 'b':
+                    break
+                elif test_input(input_city_num)==1:
+                    print('您输入的不是数字！！！')
+                elif int(input_city_num) not in city_num_list:
+                    print('输入编号不在列表范围内！！！')
+                else:
+                    #标号对应的城市名称
+                    city_name=city_num_list[int(input_city_num)]
+                    #获取县级城市list
+                    county_list=entire_dict[p_name][city_name]
+                    while flag=='false':
+                        a = 1
+                        for i in county_list:
+                            print(a,i)
+                            a +=1
+                        input_cent=input('请输入b返回上级菜单，输入q退出，输入s返回省级菜单：').strip()
+                        if input_cent =='b':
+                            break
+                        elif input_cent=='q':
+                            print('退出了！！！')
+                            sys.exit(0)
+                        elif input_cent=='s':
+                            flag='true'
+                        else:
+                            if input_cent not in ['b','s','q']:
+                                print('请按提示输入，谢谢！！！')
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
