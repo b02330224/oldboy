fa3a865fa0b0a5583fa495d1fe696fec9e872bea
weiqiang <240325184@qq.com>
2016-01-05 00:46:00 +0800

    updata

--- day01/login/login.py
+++ day01/login/login.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-import json
+# -*- coding:utf-8 -*-
+
 import sys
+import json
+import getpass
+
 
 #调试用
 #info_list={'aaa':{'passwd':'123456','status':'locked'},'bbb':{'passwd':'123456','status':'unlocked'}}
@@ -21,14 +25,17 @@ print(user_info)
 
 while True:
     input_name=input('输入您的用户名：')
-    #如果用户名在字典中，继续
-    if input_name in user_info:
-        if user_info[input_name]['status']== 'unlocked':#如果用户名不是锁定状态，继续
+    #如果用户名不在字典中
+    if input_name not in user_info:
+        print('用户名不存在，请重新输入！')
+    else:
+        if user_info[input_name]['status']== 'locked':#如果用户名是锁定状态
+            print('你的用户名已经被锁定，请24小时后再试!')
+        else:
             passwd_try_times = 3
             while passwd_try_times>0:
-                input_passwd=input('请输入您的密码：')
-                 #如果用户名和密码对应，继续
-                if input_passwd == user_info[input_name]['passwd']:
+                input_passwd=getpass.getpass(prompt='请输入您的密码：')
+                if input_passwd == user_info[input_name]['passwd']:  #如果用户名和密码对应
                     print('恭喜你登录成功！')
                     sys.exit(0)
                 else:
@@ -38,13 +45,9 @@ while True:
              #如果剩余重试密码次数为0，改变用户状态为locked，同时持久化用户信息并退出！
             else:
                 user_info[input_name]['status']='locked'
-                print(user_info)
                 checkout(user_info)
                 print('输入密码错误次数太多，已被锁定，强制退出')
                 sys.exit(1)
-        else:
-            print('你的用户名已经被锁定，请24小时后再试!')
-    else:
-        print('用户名不存在，请重新输入！')
+
 
 
